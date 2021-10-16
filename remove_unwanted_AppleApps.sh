768dea262caf8f72abe37231f24d11a17b62f927
Darien <djentwistle@gmail.com>
2021-10-15 23:03:47 -0400

    Add files via upload

---
+++ remove_unwanted_AppleApps.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# Deletes unwanted bundled Apple apps on your Mac
+echo  Deletes unwanted bundled Apple apps on your Mac
+
+
+sudo rm -rf /Applications/iMovie.app
+say iMovie removed
+sudo rm -rf /Applications/GarageBand.app
+say GarageBand removed
+sudo rm -rf /Applications/Pages.app
+say Pages removed!
+sudo rm -rf /Applications/Keynote.app
+say say goodbye to keynote!
+sudo rm -rf /Applications/Numbers.app
+say there goes the numbers!
+say All done! all apps removed , exiting
+
+exit
