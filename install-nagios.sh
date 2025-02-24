b0a65360664e2cef9457c4e80b118e78b55e5d00
Darien <djentwistle@gmail.com>
2025-02-24 09:52:35 -0500

    Update install-nagios.sh

--- install-nagios.sh
+++ install-nagios.sh
@@ -2,7 +2,7 @@
 set -e
 
 ########################################
-# 1. Update system and install packages
+# 1. System Update & Package Installation
 ########################################
 echo "Updating system packages..."
 yum update -y
@@ -11,9 +11,9 @@ echo "Installing required packages..."
 yum install -y httpd php gcc glibc glibc-common gd gd-devel make net-snmp openssl wget unzip
 
 ########################################
-# 2. Create nagios user and groups
+# 2. Create Nagios User and Groups
 ########################################
-echo "Creating nagios user and configuring groups..."
+echo "Creating nagios user and nagcmd group (if they don't exist)..."
 if ! id nagios &>/dev/null; then
     useradd nagios
 fi
@@ -22,21 +22,21 @@ if ! getent group nagcmd &>/dev/null; then
     groupadd nagcmd
 fi
 
-# Add both nagios and apache users to the nagcmd group
+# Add nagios and apache (httpd) users to nagcmd group
 usermod -a -G nagcmd nagios
 usermod -a -G nagcmd apache
 
 ########################################
-# 3. Download, compile and install Nagios Core
+# 3. Download, Compile, and Install Nagios Core (v4.5.9)
 ########################################
-NAGIOS_VERSION="4.4.6"
+NAGIOS_CORE_VERSION="4.5.9"
+echo "Downloading Nagios Core version ${NAGIOS_CORE_VERSION}..."
 cd /tmp
-echo "Downloading Nagios Core version ${NAGIOS_VERSION}..."
-wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-${NAGIOS_VERSION}.tar.gz
+wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-${NAGIOS_CORE_VERSION}.tar.gz
 
 echo "Extracting Nagios Core..."
-tar zxvf nagios-${NAGIOS_VERSION}.tar.gz
-cd nagios-${NAGIOS_VERSION}
+tar zxvf nagios-${NAGIOS_CORE_VERSION}.tar.gz
+cd nagios-${NAGIOS_CORE_VERSION}
 
 echo "Configuring Nagios Core..."
 ./configure --with-command-group=nagcmd
@@ -52,21 +52,23 @@ make install-config
 make install-webconf
 
 ########################################
-# 4. Configure the Nagios web interface
+# 4. Configure the Nagios Web Interface
 ########################################
 echo "Setting up Nagios web interface user (nagiosadmin)..."
 read -p "Enter password for nagiosadmin: " NAGIOS_PASS
 htpasswd -cb /usr/local/nagios/etc/htpasswd.users nagiosadmin "$NAGIOS_PASS"
 
 ########################################
-# 5. Download, compile and install Nagios Plugins
+# 5. Download, Compile, and Install Nagios Plugins (v2.4.3)
 ########################################
-PLUGINS_VERSION="2.3.3"
+NAGIOS_PLUGINS_VERSION="2.4.3"
+echo "Downloading Nagios Plugins version ${NAGIOS_PLUGINS_VERSION}..."
 cd /tmp
-echo "Downloading Nagios Plugins version ${PLUGINS_VERSION}..."
-wget https://nagios-plugins.org/download/nagios-plugins-${PLUGINS_VERSION}.tar.gz
-tar zxvf nagios-plugins-${PLUGINS_VERSION}.tar.gz
-cd nagios-plugins-${PLUGINS_VERSION}
+wget https://nagios-plugins.org/download/nagios-plugins-${NAGIOS_PLUGINS_VERSION}.tar.gz
+
+echo "Extracting Nagios Plugins..."
+tar zxvf nagios-plugins-${NAGIOS_PLUGINS_VERSION}.tar.gz
+cd nagios-plugins-${NAGIOS_PLUGINS_VERSION}
 
 echo "Configuring Nagios Plugins..."
 ./configure --with-nagios-user=nagios --with-nagios-group=nagios
@@ -78,7 +80,7 @@ echo "Installing Nagios Plugins..."
 make install
 
 ########################################
-# 6. Enable and start services
+# 6. Enable and Start Services
 ########################################
 echo "Enabling and starting Apache (httpd) and Nagios services..."
 systemctl enable httpd
@@ -88,14 +90,13 @@ systemctl enable nagios
 systemctl start nagios
 
 ########################################
-# 7. Configure iptables to allow HTTP/HTTPS traffic
+# 7. Configure iptables Firewall (Allowing HTTP/HTTPS Traffic)
 ########################################
 echo "Configuring iptables to allow HTTP (port 80) and HTTPS (port 443) traffic..."
 iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 iptables-save > /etc/sysconfig/iptables
 
-# Restart iptables service if it is active
 if systemctl is-active --quiet iptables; then
     systemctl restart iptables
 fi
@@ -103,5 +104,5 @@ fi
 ########################################
 # Final Message
 ########################################
-echo "Nagios Core installation and configuration complete."
-echo "You can access the Nagios web interface at: http://<your_server_ip>/nagios"
+echo "Nagios Core installation (v${NAGIOS_CORE_VERSION}) with Plugins (v${NAGIOS_PLUGINS_VERSION}) is complete."
+echo "Access the Nagios web interface at: http://<your_server_ip>/nagios"
